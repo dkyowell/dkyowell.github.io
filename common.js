731e79ff9969e99419bf31ae963d4ddea9b5c73d
David Yowell <dkyowell@me.com>
2025-05-07 11:54:40 -0500

    Update

--- common.js
+++ common.js
@@ -1,22 +1,11 @@
-const menuButton = document.getElementById('menu-button');
-
-menuButton.addEventListener('click', openMenu);
+document.getElementById('menu-button').addEventListener('click', openMenu);
 window.addEventListener('beforeunload', closeMenu);
 window.addEventListener('resize', closeMenu);
 
 function openMenu() {
-	const header = document.querySelector('#header');
-	const bottom = document.querySelector('#bottombar');
-	console.log(bottom)
-	if (header.classList.contains('menu-icon-active')) {
-		header.classList.remove('menu-icon-active');
-		bottom.classList.remove('menu-icon-active');
-	} else {
-		header.classList.add('menu-icon-active');
-		bottom.classList.add('menu-icon-active');
-	}
+	document.body.classList.toggle('popdown-active');
 }
 
 function closeMenu() {
-	menuButton.classList.remove('hidden');
+	document.body.classList.remove('popdown-active');
 }
